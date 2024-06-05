1fca93769bd99aa2f51b6292d2676ececa4451ff
Waliston <wesantosdev@gmail.com>
2024-06-05 18:26:36 -0300

    update

--- back/config/db_connection.js
+++ back/config/db_connection.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 
 //Set up default mongoose connection
 let con = "mongodb://127.0.0.1:27017/"
-var mongoDB = con;
+let prod = "mongodb+srv://we:4002892233513341@cluster0.sdsh59o.mongodb.net/"
+var mongoDB = prod;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 
 //Get the default connection
